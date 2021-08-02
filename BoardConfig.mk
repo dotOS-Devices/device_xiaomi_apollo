cb3f68f0b2cdec22ee79fdb39aca5d39db28499d
Bartłomiej Rudecki <ejbtrd@gmail.com>
2021-08-03 00:44:12 +0200

    [SQUASH] apollo: Move NFC manifest entries out of nfc SKU
    This reverts commits a07d4db25c57d55711a47a2735f8b791fb45e995, b17e9488d854e22d04fb0d68d2a20a4919f32052, a40d578b3f76f06175e52481e8e069f6d3ef94f3, 386a320cdc7917ddcf21df585b0b0d06a80b5fec, f22f71cc929e9ed63fb0aa26eb68d083160c6d2d.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -82,8 +82,6 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
-ODM_MANIFEST_SKUS += nfc
-ODM_MANIFEST_NFC_FILES := $(DEVICE_PATH)/manifest_nfc.xml
 
 # Kernel
 ifeq ($(PRODUCT_VIRTUAL_AB_OTA),true)
