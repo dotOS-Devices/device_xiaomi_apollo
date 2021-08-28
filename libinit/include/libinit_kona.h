LineageOS/android_device_xiaomi_sm8250-common

sm8250-common: Initial variant handler libinit
* This libinit has been made to commonize device variants props handling

Change-Id: Iab68ff451ab1d6e861fb4cda4ef07fad3123ecde

libinit/include/libinit_kona.h
@@ -1,0 +1,23 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <string>
+
+typedef struct variant_info {
+    std::string sku_value;
+
+    std::string brand;
+    std::string device;
+    std::string model;
+    std::string build_description;
+    std::string build_fingerprint;
+} variant_info_t;
+
+void search_variant(const variant_info_t variants[]);
+void set_variant_props(const variant_info_t variant);
+
+void property_override(char const prop[], char const value[], bool add = true);
+void set_ro_build_prop(const std::string &prop, const std::string &value, bool product = false);

dotOS-Devices/device_xiaomi_apollo

libinit/include/libinit_kona.h
@@ -1,0 +1,26 @@
+/*
+ * Copyright (C) 2021 The LineageOS Project
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <string>
+#include <vector>
+
+typedef struct variant_info {
+    std::string prop_key;
+    std::string prop_value;
+
+    std::string brand;
+    std::string device;
+    std::string marketname;
+    std::string model;
+    std::string build_description;
+    std::string build_fingerprint;
+} variant_info_t;
+
+void search_variant(const std::vector<variant_info_t> variants);
+void set_variant_props(const variant_info_t variant);
+
+void property_override(char const prop[], char const value[], bool add = true);
+void set_ro_build_prop(const std::string &prop, const std::string &value, bool product = false);