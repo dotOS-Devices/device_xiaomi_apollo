16797b3e3f9243aaba1164da5d0bbfed4ed26e84
Bartłomiej Rudecki <ejbtrd@gmail.com>
2021-10-07 10:18:30 +0200

    apollo: Drop LiveDisplay and Trust

--- device.mk
+++ device.mk
@@ -245,10 +245,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.xiaomi_kona
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
@@ -395,10 +391,6 @@ PRODUCT_BOOT_JARS += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
-
-# Trust
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
 
 # USB
 PRODUCT_PACKAGES += \
