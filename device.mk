a4905d6b39d37859852ef5f3b78b4a5650a1301e
Bartłomiej Rudecki <ejbtrd@gmail.com>
2021-10-11 17:45:25 +0200

    [TMP] apollo: Android S

--- device.mk
+++ device.mk
@@ -238,8 +238,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     org.ifaa.android.manager
 
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
+#PRODUCT_BOOT_JARS += \
+#    org.ifaa.android.manager
 
 # IPACM
 PRODUCT_PACKAGES += \
@@ -465,8 +465,8 @@ PRODUCT_PACKAGES += \
     libnl \
     libwfdaac_vendor
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
+#PRODUCT_BOOT_JARS += \
+#    WfdCommon
 
 PRODUCT_SHIPPING_API_LEVEL := 29
 
